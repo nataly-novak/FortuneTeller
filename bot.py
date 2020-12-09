9679a67534c26d35669a4f455eeb189f0d047e8b
Nataly Novak <miss.novak137@yandex.com>
2020-12-09 17:04:50 +0300

    Removed everything I do not need.

--- bot.py
+++ bot.py
@@ -14,19 +14,7 @@ conn = psycopg2.connect(DATABASE_URL, sslmode='require')
 
 cur = conn.cursor()
 #cur.execute("DROP TABLE inter")
-cur.execute("select exists(select * from information_schema.tables where table_name=%s)", ('quotes',))
-check = (cur.fetchone()[0])
-print(check)
-if not check:
-    cur.execute('''CREATE TABLE quotes (
-    NUM INT  NOT NULL,
-    QUOT CHAR(200)  NOT NULL
-    );''')
-    print("Table created successfully")
-    cur.execute("ALTER TABLE quotes ADD CONSTRAINT test_pkey PRIMARY KEY (QUOT);")
-else:
-    print('No need')
-conn.commit()
+
 
 print('koai')
 
@@ -46,21 +34,6 @@ else:
 conn.commit()
 
 load_dotenv()
-f = open("quotes.txt", "r")
-f1 = f.readlines()
-cnt = 0
-for i in f1:
-    cur.execute("INSERT INTO quotes VALUES (%s,%s) ON CONFLICT (QUOT) DO NOTHING ;",(cnt,i[:-1]))
-    cnt += 1
-    print(cnt)
-cur.execute("SELECT NUM, QUOT from quotes")
-rows = cur.fetchall()
-for j in rows:
-    print(j)
-
-n = len(f1)
-print(n)
-f.close()
 
 f = open("international", "r")
 f1 = f.readlines()
@@ -85,17 +58,6 @@ TOKEN = os.getenv('DISCORD_TOKEN')
 bot = commands.Bot(command_prefix='#')
 
 
-@bot.command(name='c', help = "generates random quotes")
-async def cook(ctx):
-    cur.execute("SELECT MAX(NUM) FROM quotes;")
-    r = str(cur.fetchone())
-    rr = int(r[1:-2])
-    print(rr)
-    cur.execute("SELECT QUOT FROM quotes WHERE NUM = %s",([random.randrange(rr+1)]))
-    resp = str(cur.fetchone())
-    response = resp[2:-3].strip()
-    print(response)
-    await ctx.send(response)
 @bot.command(name='q', help = "generates random quotes with translation")
 async def cookin(ctx):
     cur.execute("SELECT MAX(NUM) FROM inter;")
@@ -112,21 +74,6 @@ async def cookin(ctx):
     print(response)
     await ctx.send(response)
 
-@bot.command(name='w' , help='Adds a quote. Use quotes around it to enter')
-async def writ(ctx, line):
-    cur.execute("SELECT MAX(NUM) FROM quotes;")
-    a = str(cur.fetchone())
-    if a == 'None':
-        ar = 1
-    else:
-        ar = int(a[1:-2])+1
-    print(ar)
-    cur.execute("INSERT INTO quotes VALUES (%s,%s) ON CONFLICT (QUOT) DO NOTHING ;",(ar,line))
-    conn.commit()
-    cur.execute("SELECT NUM, QUOT from quotes")
-    rows = cur.fetchall()
-    for j in rows:
-        print(j)
 
 @bot.command(name='a' , help='Adds a quote. Use quotes around both quote and translation')
 async def itl(ctx, line, trans =""):
