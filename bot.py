b5a6fba01c1f8faaaf0644ebd7c9798fae6b76d4
Nataly Novak <miss.novak137@yandex.com>
2020-12-09 19:17:49 +0300

    kicker

--- bot.py
+++ bot.py
@@ -206,7 +206,7 @@ async def thedeck(ctx, stat, num):
 
 @bot.command(name='kick')
 async def kick(ctx):
-    a = ctx.message.author.mention
+    a = "call " + ctx.message.author.mention
     await ctx.send(a)
 
 bot.run(TOKEN)
