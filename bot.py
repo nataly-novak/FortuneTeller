5303090fb50bda4f81a394dbb5f510c09b3ee8fd
Nataly Novak <miss.novak137@yandex.com>
2020-12-09 18:24:09 +0300

    Started kicker functionality

--- bot.py
+++ bot.py
@@ -206,6 +206,6 @@ async def thedeck(ctx, stat, num):
 
 @bot.command(name='kick')
 async def kick(ctx):
-    await ctx.send(ctx.message.author.mention)
+    await ctx.send(kick(),' ',ctx.message.author.mention)
 
 bot.run(TOKEN)
